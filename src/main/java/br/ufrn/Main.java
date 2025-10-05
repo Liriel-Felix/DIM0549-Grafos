460e514c0a075e1a2312e0d83835be661edd2018
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-10-05 11:45:15 -0300

    feat: chamada da função para fazer a leitura dos arquivos na main

--- src/main/java/br/ufrn/Main.java
+++ src/main/java/br/ufrn/Main.java
@@ -1,7 +1,28 @@
-package br.ufrn.grafos;
+package br.ufrn;
+
+import br.ufrn.modelo.Grafo;
+import br.ufrn.util.DotReader;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Teste");
+        try {
+            DotReader reader = new DotReader();
+
+            System.out.println("Lendo grafo não direcionado...");
+            Grafo grafoNaoDirecionado = reader.lerArquivo("samples/undirected_graph.gv");
+
+            // TODO: Chamar algoritmo de BFS
+
+            System.out.println("\nLendo grafo direcionado...");
+            Grafo grafoDirecionado = reader.lerArquivo("samples/directed_graph.gv");
+
+            // TODO: Chamar algoritmo de DFS
+
+        } catch (IOException e) {
+            System.err.println("Ocorreu um erro ao ler o arquivo do grafo: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
